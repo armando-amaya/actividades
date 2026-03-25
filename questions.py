4617fe6e1f1191274460559c7f120241870c4d80
Armando Amaya <armandoamaya98@gmail.com>
2026-03-25 05:35:52 -0300

    Sistema de puntaje agregado

--- questions.py
+++ questions.py
@@ -14,6 +14,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+puntaje = 0
 
 print("¡Bienvenido al Ahorcado!")
 print()
@@ -30,7 +31,9 @@ while attempts > 0:
 
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
+        puntaje += 6
         print("¡Ganaste!")
+        print(f"Puntaje final: {puntaje}")
         break
 
     print(f"Intentos restantes: {attempts}")
@@ -47,6 +50,7 @@ while attempts > 0:
         else:
             guessed.append(letter)
             attempts -= 1
+            puntaje -= 1
             print("Esa letra no está en la palabra.")
     else:
         print("Entrada no válida. Ingresa solo una letra (a-z).")
@@ -54,4 +58,6 @@ while attempts > 0:
     print()
 
 else:
+    puntaje = 0
     print(f"¡Perdiste! La palabra era: {word}")
+    print(f"Puntaje final: {puntaje}")
